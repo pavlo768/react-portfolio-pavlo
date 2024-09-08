99f622be3b99a290d64cc2dc4b1f71374dfdf46c
Pavlo <155533645+pavlo768@users.noreply.github.com>
2024-09-08 19:15:24 +0300

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,10 +8,19 @@ const Contact = () => {
           whileInView={{ opacity: 1, y: 0 }}  
           initial={{ opacity: 0, y: -100 }}  
           transition={{ duration: 0.5 }}
-          className="my-10 text-center text-4xl">Get in Touch</motion.h1 >
+          className="my-10 text-center text-4xl">Get in Touch
+        </motion.h1 >
         <div className="text-center tracking-tighter">
-            <p className="my-4">{CONTACT.address}</p>
-            <p className="my-4">{CONTACT.phoneNo}</p>
+            <motion.p 
+               whileInView={{ opacity: 1, y: 0 }}  
+               initial={{ opacity: 0, x: -100 }}  
+               transition={{ duration: 1 }}
+            className="my-4">{CONTACT.address}</motion.p>
+            <motion.p 
+              whileInView={{ opacity: 1, y: 0 }}  
+              initial={{ opacity: 0, y: 100 }}  
+              transition={{ duration: 0.5 }}
+            className="my-4">{CONTACT.phoneNo}</motion.p>
            <a href="#" className="border-b ">{CONTACT.email}</a>
         </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,11 @@ className="my-20 text-center text-4xl">Projects</motion.h1>
       className="mb-6 rounded"
       />
     </motion.div>
-    <motion.div className="w-full max-w-xl lg:w-3/4">
+    <motion.div 
+      whileInView={{ opacity: 1, y: 0 }}  
+      initial={{ opacity: 0, y: 100 }}  
+      transition={{ duration: 1 }}
+    className="w-full max-w-xl lg:w-3/4">
     <h6 className="mb-2 font-semibold">{project.title}</h6>
     <p className="mb-4 text-neutral-500">{project.description}</p>
     {project.technologies.map((tech, index) => (
