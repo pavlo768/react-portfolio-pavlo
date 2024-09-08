304d1d3eba30ba396543aab35b983a031ca05674
Pavlo <155533645+pavlo768@users.noreply.github.com>
2024-09-08 22:28:20 +0300

    new-2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             <motion.div
             whileInView={{opacity: 1,x:0}}
             initial={{opacity: 0, x:-100}}
-            transition={{duration: 0.5}}
+            transition={{duration: 0.2}}
             className="w-full lg:w-1/2 lg:p-8">
             <div className="flex items-cente justify-center">
                 <img className="rounded-2xl" src={aboutImg} alt="about" />
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,47 @@
-import { CONTACT } from "../information"
-import { motion } from "framer-motion";
+import { CONTACT } from "../information";  
+import { motion } from "framer-motion";  
 
-const Contact = () => {
-  return (
-    <div className="border-b border-neutral-950 pd-20">
-        <motion.h1 
+const Contact = () => {  
+  return (  
+    <div className="border-b border-neutral-950 py-20 flex flex-col items-center">  
+      <motion.h1   
+        whileInView={{ opacity: 1, y: 0 }}  
+        initial={{ opacity: 0, y: -100 }}  
+        transition={{ duration: 0.5 }}  
+        className="my-10 text-center text-4xl"  
+      >  
+        Зв'яжіться з нами  
+      </motion.h1>  
+      <div className="text-center tracking-tighter">  
+        <motion.p   
           whileInView={{ opacity: 1, y: 0 }}  
-          initial={{ opacity: 0, y: -100 }}  
-          transition={{ duration: 0.5 }}
-          className="my-10 text-center text-4xl">Get in Touch
-        </motion.h1 >
-        <div className="text-center tracking-tighter">
-            <motion.p 
-               whileInView={{ opacity: 1, y: 0 }}  
-               initial={{ opacity: 0, x: -100 }}  
-               transition={{ duration: 1 }}
-            className="my-4">{CONTACT.address}</motion.p>
-            <motion.p 
-              whileInView={{ opacity: 1, y: 0 }}  
-              initial={{ opacity: 0, y: 100 }}  
-              transition={{ duration: 0.5 }}
-            className="my-4">{CONTACT.phoneNo}</motion.p>
-           <a href="#" className="border-b ">{CONTACT.email}</a>
-        </div>
-    </div>
-  )
-}
+          initial={{ opacity: 0, x: 100 }}  
+          transition={{ duration: 1 }}  
+          className="my-4"  
+        >  
+          {CONTACT.address}  
+        </motion.p>  
+        
+        <motion.p   
+          whileInView={{ opacity: 1, y: 0 }}  
+          initial={{ opacity: 0, y: 100 }}  
+          transition={{ duration: 0.5 }}  
+          className="my-4"  
+        >  
+          {CONTACT.phoneNo}  
+        </motion.p>  
+        <motion.a   
+          whileInView={{ opacity: 1, y: 0 }}  
+          initial={{ opacity: 0, y: 100 }}  
+          transition={{ duration: 0.5 }}  
+          href="#"   
+          className="border-b my-4"  
+        >  
+          {CONTACT.email}  
+        </motion.a>  
+      </div>  
+    </div>  
+  );  
+}  
 
-export default Contact
+export default Contact;
